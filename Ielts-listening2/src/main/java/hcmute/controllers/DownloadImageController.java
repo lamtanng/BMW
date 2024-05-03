9da1d65250588fdc0c45f5877990956477a58b16
Ngọc Thương <waytnt17523@gmail.com>
2024-05-03 17:09:46 +0700

    Application Error Disclosure

--- Ielts-listening2/src/main/java/hcmute/controllers/DownloadImageController.java
+++ Ielts-listening2/src/main/java/hcmute/controllers/DownloadImageController.java
@@ -28,72 +28,107 @@ public class DownloadImageController extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-    public static final String DIR = "C:\\ImagesWeb";
+	public static final String DIR = "C:\\ImagesWeb";
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String fileName = req.getParameter("fname");
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String fileName = req.getParameter("fname");
 
-        // Kiểm tra tính hợp lệ của fileName
-        if (isValidFileName(fileName)) {
-            // Tiếp tục xử lý tệp tin với đường dẫn đã được kiểm tra.
-        	// Sau khi xây dựng đường dẫn tuyệt đối đến tệp, mã kiểm tra xem đường dẫn này có bắt đầu bằng thư mục gốc DIR hay không. Nếu không, đường dẫn tệp sẽ không nằm trong phạm vi của DIR và được coi là không hợp lệ.
-            File file = new File(DIR, fileName).getCanonicalFile();
-            if (Files.exists(file.toPath()) && file.getAbsolutePath().startsWith(DIR)) {
-                String url = req.getRequestURI().toString();
-                if (url.contains("image")) {
-                    resp.setContentType("image/jpeg");
-                    try (InputStream inputStream = new FileInputStream(file)) {
-                        OutputStream outputStream = resp.getOutputStream();
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = inputStream.read(buffer)) != -1) {
-                            outputStream.write(buffer, 0, bytesRead);
-                        }
-                    }
-                } else if (url.contains("audio")) {
-                    resp.setContentType("audio/mpeg");
-                    resp.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
-                    resp.setHeader("Accept-Ranges", "bytes");
-                    long fileSize = file.length();
-                    resp.setContentLengthLong(fileSize);
-                    try (InputStream inputStream = new FileInputStream(file)) {
-                        IOUtils.copy(inputStream, resp.getOutputStream());
-                    }
-                } else if (url.contains("video")) {
-                    resp.setContentType("video/mp4");
-                    long fileSize = file.length();
-                    resp.setContentLengthLong(fileSize);
-                    try (InputStream inputStream = new FileInputStream(file)) {
-                        IOUtils.copy(inputStream, resp.getOutputStream());
-                    }
-                }
-            } else {
-                sendNotFoundResponse(resp);
-            }
-        } else {
-            sendBadRequestResponse(resp);
-        }
-    }
+		// Kiểm tra tính hợp lệ của fileName
+		if (isValidFileName(fileName)) {
+			try {
+				File file = new File(DIR, fileName).getCanonicalFile();
+				if (Files.exists(file.toPath()) && file.getAbsolutePath().startsWith(DIR)) {
+					String url = req.getRequestURI().toString();
+					if (url.contains("image")) {
+						resp.setContentType("image/jpeg");
+						try (InputStream inputStream = new FileInputStream(file)) {
+							OutputStream outputStream = resp.getOutputStream();
+							byte[] buffer = new byte[4096];
+							int bytesRead;
+							while ((bytesRead = inputStream.read(buffer)) != -1) {
+								outputStream.write(buffer, 0, bytesRead);
+							}
+						}
+					} else if (url.contains("audio")) {
+						resp.setContentType("audio/mpeg");
+						resp.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+						resp.setHeader("Accept-Ranges", "bytes");
+						long fileSize = file.length();
+						resp.setContentLengthLong(fileSize);
+						try (InputStream inputStream = new FileInputStream(file)) {
+							IOUtils.copy(inputStream, resp.getOutputStream());
+						}
+					} else if (url.contains("video")) {
+						resp.setContentType("video/mp4");
+						long fileSize = file.length();
+						resp.setContentLengthLong(fileSize);
+						try (InputStream inputStream = new FileInputStream(file)) {
+							IOUtils.copy(inputStream, resp.getOutputStream());
+						}
+					} else {
+						resp.setContentType("text/plain");
+						resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+						resp.getWriter().println("Unsupported file type.");
+					}
+				} else {
+					sendNotFoundResponse(resp);
+				}
+			} catch (IOException e) {
+				sendBadRequestResponse(resp);
+			} catch (Exception e) {
+				sendBadRequestResponse(resp);
+			}
+		} else {
+			sendBadRequestResponse(resp);
+		}
+	}
 
-    // Phương thức kiểm tra tính hợp lệ của tên tệp
-    // mã kiểm tra xem tên tệp có chứa chuỗi ".." không
-    private boolean isValidFileName(String fileName) {
-        // Kiểm tra xem fileName không null và không chứa chuỗi ".."
-        return fileName != null && !fileName.contains("..");
-    }
+	// Phương thức gửi phản hồi lỗi
+	private void sendErrorResponse(HttpServletResponse resp, String errorMessage) throws IOException {
+		resp.setContentType("text/plain");
+		resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		resp.getWriter().println(errorMessage);
+	}
 
-    // Phương thức gửi mã lỗi 404 (Not Found)
-    private void sendNotFoundResponse(HttpServletResponse resp) throws IOException {
-        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        resp.getWriter().println("<html><body><p>NOT FOUND</p></body></html>");
-    }
+	// Phương thức gửi phản hồi lỗi khi yêu cầu không hợp lệ
+	private void sendBadRequestResponse(HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		resp.getWriter().println("Bad request.");
+	}
 
-    // Phương thức gửi mã lỗi 400 (Bad Request)
-    // 
-    private void sendBadRequestResponse(HttpServletResponse resp) throws IOException {
-        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-    }
+	// Phương thức gửi phản hồi lỗi khi không tìm thấy tệp tin
+	private void sendNotFoundResponse(HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		resp.getWriter().println("File not found.");
+	}
+
+	// Phương thức kiểm tra tính hợp lệ của tên tệp
+	// mã kiểm tra xem tên tệp có chứa chuỗi ".." không
+	private boolean isValidFileName(String fileName) {
+		// Kiểm tra xem fileName không null và không chứa chuỗi ".."
+		return fileName != null && !fileName.contains("..");
+	}
+
+	// Phương thức gửi mã lỗi 404 (Not Found)
+//	private void sendNotFoundResponse(HttpServletResponse resp) throws IOException {
+//		checkError(resp);
+//
+//	}
+//
+//	// Phương thức gửi mã lỗi 400 (Bad Request)
+//	//
+//	private void sendBadRequestResponse(HttpServletResponse resp) throws IOException {
+//		checkError(resp);
+//	}
+//
+//	private void checkError(HttpServletResponse resp) throws IOException {
+//		resp.setContentType("text/plain");
+//		resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//		resp.getWriter().println("Unsupported file type.");
+//	}
 //	private static final long serialVersionUID = 1L;
 //
 //	@Override
